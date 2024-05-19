080da5f1be9fbbc904e5b6840bb60cddb314009b
SuprajitGhosh <suprajit.ghosh@gmail.com>
2024-05-19 19:01:39 +0530

    GetProductById

--- src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductEndpoint.cs
@@ -1,6 +1,21 @@
 ﻿namespace Catalog.API.Products.GetProduct
 {
-    public class GetProductEndpoint
+    public record GetProductResponse(IEnumerable<Product> Products);
+    public class GetProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("GetProduct")
+                .Produces<GetProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Product")
+                .WithDescription("Get Product");
+        }
     }
 }
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Guid) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        async public Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductQueryHandler.Handle called with {@Query}", query);
+            var product = await session.LoadAsync<Product>(query.Guid, cancellationToken);
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+            return new GetProductByIdResult(product);
+        }
+    }
+}
