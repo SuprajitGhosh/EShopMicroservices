9bfeea95afa66ea97216707e2e008b7dbbb368a3
SuprajitGhosh <suprajit.ghosh@gmail.com>
2024-05-19 14:00:31 +0530

    AddMarten in program.cs docker compose

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -9,6 +9,9 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(options => {
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
