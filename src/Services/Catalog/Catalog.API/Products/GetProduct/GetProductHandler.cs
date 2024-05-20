86b49e9b63d74a94ef18ef29bce8ca68f7dffd9b
SuprajitGhosh <suprajit.ghosh@gmail.com>
2024-05-20 20:21:37 +0530

    PageNumber, PageSize in GetProductQueryHandler and GetProductEndpoint

--- src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductHandler.cs
@@ -1,15 +1,14 @@
-﻿
-namespace Catalog.API.Products.GetProduct
+﻿namespace Catalog.API.Products.GetProduct
 {
 
-    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 4) : IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
     internal class GetProductQueryHandler(IDocumentSession session)
         : IQueryHandler<GetProductsQuery, GetProductsResult>
     {
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await session.Query<Product>().ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 4, cancellationToken);
             return new GetProductsResult(products);
         }
     }
