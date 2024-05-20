1fb89cc823e270062e2e12dc994cf308476be814
SuprajitGhosh <suprajit.ghosh@gmail.com>
2024-05-20 09:20:15 +0530

    CreateProductCommandValidator

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -2,11 +2,27 @@
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult( Guid Id);
-    internal class CreateProductCommandHandler(IDocumentSession session)
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is Required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is Required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is Required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price should be greater than 0");
+        }
+    }
+    internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+            var result = await validator.ValidateAsync(command, cancellationToken);
+            var error = result.Errors.Select(x => x.ErrorMessage).ToList();
+            if (error.Any())
+            {
+                throw new ValidationException(error.FirstOrDefault());
+            }
             // business logic to create a product
             var product = new Product
             {
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
@@ -7,8 +9,9 @@ builder.Services.AddCarter(
 );
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddMarten(options => {
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
