696b979c4329e2aa9db5146260cecbd778b77306
SuprajitGhosh <suprajit.ghosh@gmail.com>
2024-05-20 14:13:01 +0530

    Global Exception Handling

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
@@ -24,4 +26,23 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.MapCarter();
+app.UseExceptionHandler(exceptionHandlerApp => exceptionHandlerApp.Run(async context => {
+    var exception = context.Features.Get<IExceptionHandlerFeature>()!.Error;
+    if (exception == null)
+    {
+        return;
+    }
+    var problemDetails = new Microsoft.AspNetCore.Mvc.ProblemDetails
+    {
+        Title = exception.Message,
+        Status = StatusCodes.Status500InternalServerError,
+        Detail = exception.StackTrace
+    };
+    var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+    logger.LogError(exception, exception.Message);
+    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+    context.Response.ContentType = "application/problem+json";
+
+    await context.Response.WriteAsJsonAsync(problemDetails);
+}));
 app.Run();
