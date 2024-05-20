e1e42c2ee25b1139f176e03be90e6b696c140f99
SuprajitGhosh <suprajit.ghosh@gmail.com>
2024-05-20 18:48:46 +0530

    CustomeExceptionHandler

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -7,4 +7,5 @@ global using Catalog.API.Models;
 global using Catalog.API.Exceptions;
 global using FluentValidation;
 global using BuildingBlocks.Behaviours;
+global using BuildingBlocks.Exceptions.Handler;
 //global using BuildingBlocks.Extentions;
