7042e8b6e2041ab67af16878eb00acffee8a6674
SuprajitGhosh <suprajit.ghosh@gmail.com>
2024-05-26 17:43:52 +0530

    CachedBasketRepository with related configuration

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,6 @@
+using BuildingBlocks.Exceptions.Handler;
+using Microsoft.Extensions.Caching.Distributed;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services 
@@ -17,10 +20,23 @@ builder.Services.AddMarten(option => {
     option.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+//builder.Services.AddScoped<IBasketRepository>(provider => { 
+//    var basketRepository = provider.GetService<BasketRepository>();
+//    var distributedCache = provider.GetRequiredService<IDistributedCache>();
+//    return new CachedBasketRepository(basketRepository!, distributedCache);
+//});
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+/*
+ * what is decorate ?
+ */
+builder.Services.AddStackExchangeRedisCache(optionss => {
+    optionss.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+builder.Services.AddExceptionHandler<CustomeExceptionHandler>();
 
 var app = builder.Build();
 
 app.MapCarter();
 //Configure the HTTP request pipeline
-
+app.UseExceptionHandler(options => { });
 app.Run();
