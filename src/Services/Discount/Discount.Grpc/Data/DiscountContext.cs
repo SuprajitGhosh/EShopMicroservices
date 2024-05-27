f82085d56630fab6b9fc974ee1df9130c1267f3c
SuprajitGhosh <suprajit.ghosh@gmail.com>
2024-05-27 15:06:54 +0530

    DBContext and Auto Migration

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -7,7 +7,13 @@ namespace Discount.Grpc.Data
         public DbSet<Coupon> coupons { get; set; } = default!;
         public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
         {
-            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+                new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+                );
         }
     }
 }
--- src/Services/Discount/Discount.Grpc/Migrations/DiscountContextModelSnapshot.cs
+++ src/Services/Discount/Discount.Grpc/Migrations/DiscountContextModelSnapshot.cs
@@ -36,6 +36,22 @@ namespace Discount.Grpc.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("coupons");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Amount = 150,
+                            Description = "IPhone Discount",
+                            ProductName = "IPhone X"
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Amount = 100,
+                            Description = "Samsung Discount",
+                            ProductName = "Samsung 10"
+                        });
                 });
 #pragma warning restore 612, 618
         }
